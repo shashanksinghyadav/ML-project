398f4b2f124a64815487cc485861cd5a90ca4d12
shashank singh <shashank.eee22@iitbhu.ac.in>
2025-03-15 23:45:16 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from data_transformation import DataTransformation
+
 @dataclass # can directly define class variables
 class DataIngestionConfig:
    train_data_path:str=os.path.join('artifacts','train.csv')# train data will be saved here
@@ -32,6 +34,9 @@ class DataIngestion:
          )
       except Exception as e:
          raise CustomException(e,sys)
-if __name__=="__main__":
-   obj=DataIngestion()
-   obj.initiate_data_ingestion()
+# if __name__=="__main__":
+   
+#     obj=DataIngestion()
+#     train_data,test_data=obj.initiate_data_ingestion()
+#     data_transformation=DataTransformation()
+#     data_transformation.initiate_data_transformation(train_data,test_data)
