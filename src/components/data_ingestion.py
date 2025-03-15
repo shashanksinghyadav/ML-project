510c538cabbcf7ea2d88644e4b96edce1e8c312f
shashank singh <shashank.eee22@iitbhu.ac.in>
2025-03-16 02:56:55 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,8 +5,8 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from data_transformation import DataTransformation
-
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from model_trainer import ModelTrainer,ModelTrainerConfig
 @dataclass # can directly define class variables
 class DataIngestionConfig:
    train_data_path:str=os.path.join('artifacts','train.csv')# train data will be saved here
@@ -23,7 +23,7 @@ class DataIngestion:
          os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
          df.to_csv(self.ingestion_config.raw_data_path)
          logging.info("train test split initiated")
-         train_set,test_set=train_test_split(df,test_size=0.2,random_state=43)
+         train_set,test_set=train_test_split(df,test_size=0.2,random_state=41)
          train_set.to_csv(self.ingestion_config.train_data_path)
          test_set.to_csv(self.ingestion_config.test_data_path)
          logging.info("ingestion of data is completed")
@@ -34,9 +34,11 @@ class DataIngestion:
          )
       except Exception as e:
          raise CustomException(e,sys)
-# if __name__=="__main__":
+if __name__=="__main__":
    
-#     obj=DataIngestion()
-#     train_data,test_data=obj.initiate_data_ingestion()
-#     data_transformation=DataTransformation()
-#     data_transformation.initiate_data_transformation(train_data,test_data)
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
